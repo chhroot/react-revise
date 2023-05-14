ddea89039effa60356a69ba25d1bb20ca068ee26
Amruth <amruthkiran47@gmail.com>
2023-05-14 16:49:27 +0530

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,1 +1,17 @@
-function App()
+import ListGroup from "./components/ListGroup";
+import Alert from "./components/alert";
+import Button from "./components/Button";
+import { useState } from "react";
+function App() {
+  const [alertVisible, setAlertVisibilty] = useState(false);
+  return (
+    <div>
+      { alertVisible && <Alert>My Alert</Alert>}
+      <Button color="secondary" onClick={() => setAlertVisibilty(true)}>
+        My Button
+      </Button>
+    </div>
+  );
+}
+
+export default App;
